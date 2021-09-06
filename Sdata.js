ebff6e90278bd09020c0b3de7d8debdd8889bafb
Gagan pal <89453636+Gaganpa@users.noreply.github.com>
2021-09-05 21:25:12 -0700

    Add files via upload

---
+++ Sdata.js
@@ -0,0 +1,33 @@
+import web from "../src/images/img1.jpg";
+import app from "../src/images/img7.jpg";
+import android from "../src/images/img7.jpg";
+import digital from "../src/images/img1.jpg";
+import marketing from "../src/images/img6.jpg";
+import software from "../src/images/img2.jpg";
+const Sdata = [
+{
+imgsrc:web,
+title:"web development",
+},
+{
+imgsrc:app,
+title:"app development",
+},
+{
+imgsrc:android,
+title:"android development",
+},
+{
+imgsrc:digital,
+title:"digital development",
+},
+{
+imgsrc:marketing,
+title:"marketing development",
+},
+{
+imgsrc:software,
+title:"software development",
+},
+];
+export default Sdata;
